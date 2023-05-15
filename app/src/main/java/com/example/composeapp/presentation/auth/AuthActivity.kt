0f58b61b72706406952a30bd3947fdd482e5c971
eslam.elmadboly <eslam.elmadboly@btech.com>
2023-05-15 11:14:22 +0200

    add splash screen integration

--- app/src/main/java/com/example/composeapp/presentation/auth/AuthActivity.kt
+++ app/src/main/java/com/example/composeapp/presentation/auth/AuthActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.composeapp.presentation.auth.navigation.AuthNavigation
 import com.example.composeapp.presentation.ui.theme.ComposeAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class AuthActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             ComposeAppTheme {
                 Surface(
